d077fc7a2e0dc82bbf01178783205b3946e54026
Yildiz Zor Unlu <yildizzor@gmail.com>
2023-09-23 22:32:17 +0200

    game is done without animals

--- js/obstacle.js
+++ js/obstacle.js
@@ -1,18 +1,32 @@
 class Obstacle extends Component {
-  constructor(gameScreen, right) {
-    super(gameScreen, 100, 60, 60, "./images/obstacle.png");
+  constructor(gameScreen, height, left, groundLevel, gravity, speed) {
+    super(gameScreen, height, 60, 60, left);
+    this.createImgElement("./images/obstacle.png");
 
-    this.right = 100;
-    this.element.style.right = `${this.right}px`;
-  }
+    this.groundLevel = groundLevel;
+    this.gravity = gravity;
+    this.velocity = 0;
+    this.distance = 0;
 
-  updatePosition() {
-    this.element.style.right = `${this.right}px`;
+    this.speed = speed;
   }
 
   move() {
-    this.right += 3;
+    this.left -= this.speed;
+    this.distance += this.speed;
+
+    if (this.bottom > this.groundLevel) {
+      this.velocity += this.gravity;
+      this.bottom -= this.velocity;
+    } else {
+      this.velocity = 0;
+      this.bottom = this.groundLevel;
+    }
 
     this.updatePosition();
+  }
+
+  getDistance() {
+    return this.distance;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
 window.onload = () => {
   const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
 
   let game;
 
   startButton.addEventListener("click", () => {
-    startGame();
-  });
-
-  function startGame() {
     console.log("GAME STARTED");
 
     game = new Game();
-
     game.start();
-  }
+  });
+
+  restartButton.addEventListener("click", function () {
+    location.reload();
+  });
+
+  window.addEventListener("keydown", (event) => {
+    const keyPressed = event.key;
+
+    switch (keyPressed) {
+      case "ArrowUp":
+      case " ":
+        event.preventDefault();
+        game.rider.setJumping(true);
+        break;
+    }
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target.id === "game-screen") {
+      game.rider.setJumping(true);
+    }
+  });
 };
